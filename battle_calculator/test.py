3cbcac69af622460267f27a0dd76deda5b911d1e
dominic <dominic@pop-os.localdomain>
2021-04-09 00:07:23 -0500

    added caching of recursive branches

--- battle_calculator/test.py
+++ battle_calculator/test.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def hit_distribution(attacks):
     # initialize empty dictionary with correct shape
     result = {i+1:{j:0 for j in range(i+2)} for i in range(len(attacks))}
@@ -19,41 +20,59 @@ def hit_distribution(attacks):
     return result    
 
 def iterate_round(a_r, d_r):
+
     # recurse down number of remaining ships
     expected_result = np.zeros(3)
     if a_r > 0 and d_r <= 0:
         expected_result[0] = 1
+        return expected_result
     elif a_r <= 0 and d_r > 0:
         expected_result[2] = 1
+        return expected_result
     elif a_r <=0 and d_r <= 0:
         expected_result[1] = 1
-    else:
-        misses = 0
-        for a_hits, a_p in a_hit_distribution[a_r].items():
-            for d_hits, d_p in d_hit_distribution[d_r].items():
-                # need to account for abilities, cards etc to calculate the new number of remaining ships
-                a_new = a_r - d_hits
-                d_new = d_r - a_hits
+        return expected_result
 
-                #### NEXT TASK
-                # add a way to check the state vs known states (particularly starting)
-                # Goal to solve 2 problems:
-                # 1 - if we have a recurring state, we can reuse the calculated probabilities 
-                    # i.e., calculation after 1-0 + 0-1 hits is the same as 1-1
-                # 2 - we can know how many attacks to remove per hit assigned (needed for implementation of ships with multiple attacks / sustain)
+    misses = 0
 
-                # figure out how to remove more than one when a hit is assigned
-                #a_prev_hits = len(a) - a_r
+    for a_hits, a_p in a_hit_distribution[a_r].items():
+        for d_hits, d_p in d_hit_distribution[d_r].items():
+            # need to account for abilities, cards etc to calculate the new number of remaining ships
+            a_new = a_r - d_hits
+            d_new = d_r - a_hits
+
+            #### NEXT TASK
+            # add a way to check the state vs known states (particularly starting)
+            # Goal to solve 2 problems:
+            # 1 - if we have a recurring state, we can reuse the calculated probabilities (DONE!)
+            # i.e., calculation after 1-0 + 0-1 hits is the same as 1-1
+            # 2 - we can know how many attacks to remove per hit assigned (needed for implementation of ships with multiple attacks / sustain)
+            # figure out how to remove more than one when a hit is assigned
+
+            ### once this is figured out, we can graduate to calculator.py and move everything to a class
 
 
-                # need to adjust probabilities for case all ships miss
-                if a_hits == 0 and d_hits == 0:
-                    misses += a_p * d_p
-                    continue
-                prior = iterate_round(a_r - d_hits, d_r - a_hits) * a_p * d_p
-                expected_result += prior
+            # need to adjust probabilities and skip recursion for case all ships miss
+            if a_hits == 0 and d_hits == 0:
+                misses += a_p * d_p
+                continue
 
-        expected_result = expected_result / (1 - misses)
+            a_n_r = a_r - d_hits
+            d_n_r = d_r - a_hits
+
+            # NEW method
+            if (a_n_r, d_n_r) in precomputed_rounds:
+                prior = precomputed_rounds[(a_n_r, d_n_r)]
+            else:
+                prior = iterate_round(a_n_r, d_n_r)
+                precomputed_rounds[(a_n_r, d_n_r)] = prior
+            
+            # OLD method (for comparison only, its a lot slower but useful for testing)
+            #prior = iterate_round(a_n_r, d_n_r)
+
+            expected_result += prior * a_p * d_p
+
+    expected_result = expected_result / (1 - misses)
 
     return expected_result
 
@@ -63,8 +82,14 @@ def iterate_round(a_r, d_r):
 
 # ships = { id: {'type' = 'cruiser', 'attacks' = [7]} ] }
 # need something to convert human inputs into this structure
-attacker = {1: {'type':'cruiser', 'attacks':[7]}, 2: {'type':'cruiser', 'attacks':[7]}, 3: {'type':'cruiser', 'attacks':[7]}}
-defender = {1: {'type':'cruiser', 'attacks':[7]}, 2: {'type':'cruiser', 'attacks':[7,7]}}
+attacker = {1: {'type':'cruiser', 'attacks':[7]}, 2: {'type':'cruiser', 'attacks':[7]}, 3: {'type':'cruiser', 'attacks':[7]}, 4: {'type':'cruiser', 'attacks':[7]},\
+    5: {'type':'cruiser', 'attacks':[7]}, 6: {'type':'cruiser', 'attacks':[7]}, 7: {'type':'cruiser', 'attacks':[7]}}#,\
+        #8: {'type':'cruiser', 'attacks':[7]}, 9: {'type':'cruiser', 'attacks':[7]}, 10: {'type':'cruiser', 'attacks':[7]},\
+        #    11: {'type':'cruiser', 'attacks':[7]}, 12: {'type':'cruiser', 'attacks':[7]}, 13: {'type':'cruiser', 'attacks':[7]}}
+defender = {1: {'type':'cruiser', 'attacks':[7]}, 2: {'type':'cruiser', 'attacks':[7]}, 3: {'type':'cruiser', 'attacks':[7]}, 4: {'type':'cruiser', 'attacks':[7]},\
+    5: {'type':'cruiser', 'attacks':[7]}, 6: {'type':'cruiser', 'attacks':[7]}, 7: {'type':'cruiser', 'attacks':[7]},\
+        8: {'type':'cruiser', 'attacks':[7]}, 9: {'type':'cruiser', 'attacks':[7]}, 10: {'type':'cruiser', 'attacks':[7]},\
+            11: {'type':'cruiser', 'attacks':[7]}, 12: {'type':'cruiser', 'attacks':[7]}, 13: {'type':'cruiser', 'attacks':[7]}}
 
 # convert to format for hit_distribution
 a = [v for x in attacker.values() for v in x['attacks']]
@@ -80,5 +105,8 @@ print(d_num_attacks_lost_after_nth_hit)
 
 a_hit_distribution = hit_distribution(a)
 d_hit_distribution = hit_distribution(d)
+
+# stores computational results for iterate_round, remember to make it a class variable
+precomputed_rounds = {}
 
 print(iterate_round(len(a), len(d)))
